7a8eac5c8d7d7dc0a858ee5e34151398330e8826
Chris Beaudoin <chrisbeaudoin.school@gmail.com>
2024-06-20 00:46:15 -0400

    Added a auditor to check for bias on the model as it is being run.

---
+++ src/audit_system.py
@@ -0,0 +1,25 @@
+# src/audit_system.py
+
+import pandas as pd
+from sklearn.metrics import classification_report
+import torch
+from model_training import LoanApprovalModel, preprocess_data
+
+def audit_model(model, data, scaler):
+    X = data.drop('loan_status', axis=1).values
+    y = data['loan_status'].values
+
+    X = torch.tensor(scaler.transform(X), dtype=torch.float32)
+    y_pred = model(X).detach().numpy().round()
+
+    report = classification_report(y, y_pred, target_names=['Rejected', 'Approved'])
+    print(report)
+
+if __name__ == "__main__":
+    data = preprocess_data('../data/loan_data.csv')
+    model = LoanApprovalModel(data.shape[1]-1)
+    model.load_state_dict(torch.load('../model/loan_approval_model.pth'))
+    model.eval()
+    scaler = torch.load('../model/scaler.pth')
+    audit_model(model, data, scaler)
+
